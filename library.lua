f280ff383ce4162c201ba5626e0cab57b5d9c603
cludetype-cloud <cludetype@gmail.com>
2026-02-10 22:40:36 +0700

    Create library.lua

---
+++ library.lua
@@ -0,0 +1,9 @@
+-- Fsvx Library
+-- Core & UI Loader
+
+local HttpService = game:GetService("HttpService")
+
+-- panggil main
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/Bxkiller/Fsvx/main/main.lua"
+))()
